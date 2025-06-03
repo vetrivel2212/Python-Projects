3d349947fb61f4bde13398336282aa0fe6197272
vetrivel2212 <vetrivel221205@gmail.com>
2025-06-03 21:36:00 +0530

    Add files via upload

---
+++ 1_madlib_game.py
@@ -0,0 +1,20 @@
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+print("Create a story by fill in the blanks")
+print(f"Today I went to a ___________ zoo.")
+adjective1 = input("Enter an adjective (description): ")
+print(f"In an exhibit, I saw a _________.")
+noun1 = input("Enter a noun (person, place, thing): ")
+print(f" It was __________ and _______!")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+print(f"I was ___________")
+adjective3 = input("Enter an adjective (description): ")
+
+print("Hey here is your story")
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}!")
+print(f"I was {adjective3}!")
